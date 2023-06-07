881cb07ac385a6923e9767bc59e5594131a61c29
Burhan Haroon <2019cs626@student.uet.edu.pk>
2023-06-07 12:54:28 +0500

    Completed

---
+++ index.js
@@ -0,0 +1,9 @@
+let navContainer = document.querySelector('.links-container');
+
+const openProfile = () => {
+  navContainer.classList.add('open');
+};
+
+const closeNavbar = () => {
+  navContainer.classList.remove('open');
+};
